19f14da9263836ef04ccab584e7a5c0dffe44166
saee <saee_saadat@outlook.com>
2021-02-18 09:55:20 +0330

    signalDevice cloud function

--- cloud/main.js
+++ cloud/main.js
@@ -1,2 +1,14 @@
 // Cloud Code entry point
 
+Parse.Cloud.define("signalDevice", async (request) => {
+    let arduino = request.params.arduino;
+    let signal = request.params.signal;
+    //TODO: handle sending the signal to the arduinos! 
+    const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+    await delay(3000);
+    if (signal == 'fail') {
+        return false
+    } else {
+        return true
+    }
+});
